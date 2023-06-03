97b5c817c10e69bdf0b6701182760dd44e6937df
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-06-04 06:55:35 +0700

    passing function via props

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["NY", "Malang", "Jakarta", "Surabaya", "Bandung", "Bali"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
