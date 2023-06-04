93c59197da1b02c884ab1889173eb5595a14f54e
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-06-04 07:20:48 +0700

    passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["NY", "Malang", "Jakarta", "Surabaya", "Bandung", "Bali"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <span>ByeWorld</span>
+      </Alert>
     </div>
   );
 }
