92efd7756dd6130b03fe7e8d4cade03f137f5e39
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-06-04 10:06:56 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const [AlertVisible, SetAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="info" onClick={() => console.log("clicked")}>
+      {AlertVisible && (
+        <Alert onClose={() => SetAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="info" onClick={() => SetAlertVisibility(true)}>
         button but from app tsx
       </Button>
     </div>
